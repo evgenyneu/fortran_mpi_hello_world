cb96b5e13afb93eb2b55fefadc4133c2a683437a
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-09-14 11:09:29 +1000

    Work

--- src/hello_mpi.f90
+++ src/hello_mpi.f90
@@ -1,3 +1,5 @@
+! Runs a program in parallel using MPI
+! (https://en.wikipedia.org/wiki/Message_Passing_Interface)
 module HelloMpi
 use mpi
 use Constants, only: OUTPUT_LENGTH
@@ -7,9 +9,22 @@ public :: hello_mpi
 
 contains
 
+!
+! Print the ID of the process and the total number of them.
+!
+! Inputs:
+! --------
+!
+! silent : if true, do not print to output (used in unit tests)
+!
+! Outputs:
+! -------
+!
+! Returns: hello world string from the process
+!
 function hello_mpi(silent) result(result)
   logical, intent(in) :: silent
-  character(len=OUTPUT_LENGTH) :: result
+  character(len=m) :: result
 
   integer :: ifail
   integer :: rank, size
@@ -19,8 +34,8 @@ function hello_mpi(silent) result(result)
   call mpi_comm_rank(MPI_COMM_WORLD, rank, ifail)
   call mpi_comm_size(MPI_COMM_WORLD, size, ifail)
 
+  ! Print the ID of the process and the total number of them
   write(result, '(a, i2, x, a, i2)') "Hello from rank", rank, "of", size
-
   if (.not. silent) write (0, *) trim(result)
 
   call mpi_finalize(ifail)
